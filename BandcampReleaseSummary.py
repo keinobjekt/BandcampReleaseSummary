a970f26b1d7da3d155904e4201834b9e5d38d3d1
TJ Hertz <tjhertz@gmail.com>
2025-12-04 22:09:16 +0100

    moved bandcamp scraping stuff to bandcamp.py

--- BandcampReleaseSummary.py
+++ BandcampReleaseSummary.py
@@ -1,11 +1,10 @@
 import pickle
-from bs4 import BeautifulSoup
 import argparse
 import datetime
 from pathlib import Path
 
 from gmail import gmail_authenticate, search_messages, get_messages, scrape_info_from_email
-
+from bandcamp import scrape_info_from_bc_page
 
 ## Settings ##
 k_no_download = False
@@ -38,92 +37,10 @@ def get_widget_string(release_id, release_url, is_track):
 
 
 
-# ------------------------------------------------------------------------ 
-# SCRAPING & PARSING, COMPILING LIST OF RELEASES, GENERATING HTML
-# ------------------------------------------------------------------------ 
-
-# Scrape metadata from the Bandcamp page of one release
-def scrape_info_from_bc_page(release):
-    release_url = release['url']
-
-    k_num_attempts = 5
-    for _ in range(k_num_attempts):
-        try:
-            from urllib.request import Request, urlopen
-            req = Request(release_url, headers={'User-Agent': 'XYZ/3.0'})
-            html_text = urlopen(req, timeout=10).read().decode()
-        except Exception as e:
-            html_text = ""
-            print(f'Error: Exception while fetching Bandcamp page at {release_url}: {e}')
-            return release
-        if html_text != "":
-            break
-        else:
-            import time
-            time.sleep(5)
-
-    if html_text == "":
-        print(f'Error: Could not load bandcamp page at {release_url}: {e}')
-        return release
-
-    soup = BeautifulSoup(html_text, 'html.parser')
-
-    # check for bandcamp 404 page
-    if "Sorry, that something isn’t here" in html_text:
-        print(f'Error: Bandcamp 404 page found at {release_url}')
-        return release
-
-    # release title
-    release['release_title'] = soup.find('h2', class_="trackTitle")
-    if release['release_title'] is not None:
-        release['release_title'] = soup.find('h2', class_="trackTitle").text.strip()        
-    else:
-        print(f'Error: Release title not found while scraping {release_url}')
-        return release
-
-    # artist name
-    def parse_artist_name(input):
-        input = input.strip()
-        if (input[0:2] != 'by'):
-            return None
-        input = input [2:]
-        return input.strip()
-
-    release['artist_name'] = soup.find('h3')
-
-    if release['artist_name'] is not None:
-        release['artist_name'] = parse_artist_name(soup.find('h3').text)
-        if release['artist_name'] == None:
-            print(f'Error parsing artist name at {release_url}')
-            return release
-    else:
-        print(f'Artist name not found at {release_url}')
-        return release
-
-    # page name 
-    release['page_name'] = soup.findAll('span', class_="title")
-    if release['page_name'] is not None:
-        release['page_name'] = release['page_name'][1].text.strip()
-    else:
-        print(f'Page name not found at {release_url}')
-        return release
-
-    # release id
-    release['release_id'] = soup.find('meta', attrs={'name':'bc-page-properties'})
-    if release['release_id'] is not None:
-        release['release_id'] = eval(release['release_id']["content"])['item_id']
-    else:
-        print (f'Release ID not found at {release_url}')
-        return release
-
-    print(f"Retrieved release data for {release['release_title']} by {release['artist_name']}")
-
-    return release
-
 
 # ------------------------------------------------------------------------ 
-# Collect all releases into a list
-def compile_release_list(raw_emails):
+# Create a list of releases with metadata
+def construct_release_list(raw_emails):
 
     print ('Parsing messages...')
     releases_unsifted = []
@@ -290,7 +207,7 @@ if __name__ == "__main__":
         with open(f"{k_data_dir}/{release_info_file}", "rb") as a_file:
             releases = pickle.load(a_file)
     else:
-        releases = compile_release_list(raw_emails)
+        releases = construct_release_list(raw_emails)
         with open(f"{k_data_dir}/{release_info_file}", "wb+") as a_file:
             pickle.dump(releases, a_file)
     
