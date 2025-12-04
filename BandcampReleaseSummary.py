9a4875eb984be3ab732df58c3df053a629ebfc4d
TJ Hertz <tjhertz@gmail.com>
2025-12-04 23:10:24 +0100

    Removed construct_release from BandcampReleaseSummary.py

--- BandcampReleaseSummary.py
+++ BandcampReleaseSummary.py
@@ -13,22 +13,6 @@ k_output_path = "output"
 
 
 # ------------------------------------------------------------------------ 
-# UTIL
-# ------------------------------------------------------------------------ 
-def construct_release(is_track=None, release_url=None, date=None, img_url=None, artist_name=None, release_title=None, page_name=None, release_id=None):
-    release = {}
-    release['img_url'] = img_url
-    release['date'] = date
-    release['artist'] = artist_name
-    release['title'] = release_title
-    release['page_name'] = page_name
-    release['url'] = release_url
-    release['release_id'] = release_id
-    release['is_track'] = is_track
-    return release
-
-
-# ------------------------------------------------------------------------ 
 # Create a list of releases with metadata
 def construct_release_list(raw_emails):
 
